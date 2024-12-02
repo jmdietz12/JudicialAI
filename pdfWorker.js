ec91ca538545b178c5e15a0de71a3162e0cd3845
Payton-6 <psilverman6@gatech.edu>
2024-12-02 02:05:35 -0500

    Update pdfWorker.js

--- pdfWorker.js
+++ pdfWorker.js
@@ -1,1 +1,9 @@
-
+self.onmessage = function(e) {
+    const { chunk } = e.data;
+    
+    const processedText = chunk
+        .replace(/\s+/g, ' ')
+        .trim()
+    
+    self.postMessage(processedText);
+};
